a954faf84542440d4fafaff352b137e104a02fcf
tenpaMk2 <wavyroom@gmail.com>
2021-09-11 17:34:06 +0900

    :rocket: initial release

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # watermark-stamper-js
+
 add watermark (alpha .png) to your images.
+
+## Usage
+
+```shell
+npm install
+```
+
+Put your watermark image (alpha png) into `watermarks` directory.
+You can put multiple images.
+
+Put your inputs image into `inputs` directory.
+
+```shell
+npm run exe
+```
+
+You can see the help.
+
+```shell
+npm run help
+```
+
+## output JPEG
+
+```shell
+node index.js -i inputs -o outputs -w watermarks --type jpg --jpegQuality 70
+```
+
+## image files
+
+What the
+[Jimp](https://www.npmjs.com/package/jimp)
+supports,
+it also supports.
+
+Be careful, it only supports **sRGB** color space.
+Not support **Adobe RGB** .
---
+++ index.js
@@ -0,0 +1,186 @@
+/**
+ * @file add watermark
+ */
+
+import jimp from "jimp";
+import path from "path";
+import fs from "fs";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
+import commander from "commander";
+
+// parse arguments
+
+/**
+ * argument parser for JPEG Quality.
+ * @param {number} value - jpeg quality `0 <= q <= 100` .
+ * @returns {number} jpeg quality `0 <= q <= 100` .
+ */
+const parseJpegQuality = (value, dummyPrevious) => {
+  const integer = parseInt(value, 10);
+  if (isNaN(integer)) {
+    throw new commander.InvalidArgumentError("Not a number.");
+  }
+  if (integer < 0) {
+    return 0;
+  }
+  if (100 < integer) {
+    return 100;
+  }
+
+  return integer;
+};
+
+/**
+ * command parser
+ */
+const program = new commander.Command();
+program
+  .requiredOption("-i, --input <path>", "input directory path.")
+  .requiredOption("-o, --output <path>", "output directory path.")
+  .requiredOption("-w, --watermark <path>", "watermark directory path.")
+  .addOption(
+    new commander.Option("-t, --type <type>", "image file type.")
+      .choices(["jpg", "png"])
+      .default("png")
+  )
+  .addOption(
+    new commander.Option(
+      "-j, --jpegQuality <numbers>",
+      "jpeg quality if output is jpeg."
+    )
+      .default(80)
+      .argParser(parseJpegQuality)
+  )
+  .addOption(
+    new commander.Option(
+      "-op, --opacity <float>",
+      "the opacity of watermark. `0 < op <= 1` "
+    ).default(0.2)
+  )
+  .parse(process.argv);
+
+/**
+ * parsed options
+ */
+const options = program.opts();
+
+/**
+ * @param {string} path
+ * @returns {boolean}
+ */
+const isImagePath = (path) =>
+  /.*\.(jpg|jpeg|png|tif|tiff)$/.test(path.toLowerCase());
+
+/**
+ * @param {Jimp} img - Jimp image
+ * @returns {boolean} `true` is landscape. `false` is portlait.
+ */
+const isLandscape = (img) => img.getWidth() > img.getHeight();
+
+/**
+ * @param {string} imageDirPath
+ * @returns {string[]}
+ */
+const getImagePaths = (imageDirPath) => {
+  const resolvedPath = path.resolve(imageDirPath);
+  const anyFiles = fs.readdirSync(resolvedPath);
+  const imageFiles = anyFiles.filter(isImagePath);
+  return imageFiles.map((imageFile) => path.resolve(imageDirPath, imageFile));
+};
+
+/**
+ * @param {string} inputPaths
+ * @param {string} watermarkPaths
+ * @param {string} outputDirPath
+ * @returns {Array}
+ */
+const createArguments = (inputPaths, watermarkPaths, outputDirPath, ext) => {
+  let myArguments = [];
+  inputPaths.forEach((inputPath) => {
+    watermarkPaths.forEach((watermarkPath) => {
+      const outputPath = path.resolve(
+        outputDirPath,
+        `${path.parse(inputPath).name}_${path.parse(watermarkPath).name}.${ext}`
+      );
+      myArguments.push([inputPath, watermarkPath, outputPath]);
+    });
+  });
+  return myArguments;
+};
+
+/**
+ * add the watermark of `watermarkPath` on the image of `inputPath` and export image of `outputPath` .
+ * @param {string} inputPath
+ * @param {string} watermarkPath
+ * @param {string} outputDirPath
+ * @returns {string}
+ */
+const addWatermark = async (inputPath, watermarkPath, outputPath) => {
+  // read images
+  const inputImage = await jimp.read(inputPath);
+  const watermarkImage = await jimp.read(watermarkPath);
+
+  // resize watermark image
+  const watermarkWidth = isLandscape(inputImage)
+    ? inputImage.getWidth() / 10
+    : inputImage.getWidth() / 5;
+  watermarkImage.resize(watermarkWidth, jimp.AUTO, jimp.RESIZE_BEZIER);
+
+  // decide watermark position
+  const padding = isLandscape(inputImage)
+    ? inputImage.getHeight() / 100
+    : inputImage.getWidth() / 100;
+  const x = padding;
+  const y = inputImage.getHeight() - watermarkImage.getHeight() - padding;
+
+  // add watermark
+  inputImage.composite(watermarkImage, x, y, {
+    mode: jimp.BLEND_SOURCE_OVER,
+    opacityDest: 1,
+    opacitySource: options.opacity,
+  });
+
+  // // resize
+  // if (isLandscape(inputImage)) {
+  //   inputImage.resize(MAX_HEIGHT_WIDTH, jimp.AUTO, jimp.RESIZE_BEZIER);
+  // } else {
+  //   inputImage.resize(jimp.AUTO, MAX_HEIGHT_WIDTH, jimp.RESIZE_BEZIER);
+  // }
+
+  // set quality
+  inputImage.quality(options.jpegQuality);
+
+  // export
+  await inputImage.writeAsync(outputPath);
+
+  return `done: ${outputPath}`;
+};
+
+const main = async () => {
+  console.log(
+    chalk.bgRed.white("Be careful!! You can only use sRGB color-space!!")
+  );
+
+  // list up image files
+  const imageFiles = getImagePaths(options.input);
+  const watermarkFiles = getImagePaths(options.watermark);
+
+  // create arguments array
+  const myArguments = createArguments(
+    imageFiles,
+    watermarkFiles,
+    path.resolve(options.output),
+    options.type
+  );
+
+  // add watermark!!
+  const promises = myArguments.map((myArgument) => addWatermark(...myArgument));
+
+  // wait all done
+  Promise.all(promises).then((val) => {
+    console.log(val);
+  });
+};
+
+await main();
